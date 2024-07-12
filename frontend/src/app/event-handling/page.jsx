e6205a7f792f3c3d0dd6443f27ee2333cdce7eae
AyushG <legendsteamprivate@gmail.com>
2024-07-12 17:26:07 +0530

    React next Js

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -2,6 +2,23 @@
 import React from 'react';
 
 const EventHandling = () => {
+
+    const previewImage = (e) =>{
+        const file = e.target.files[0];
+        // initialize file reader
+        const reader = new FileReader();
+        
+        // what do do when image is loaded
+        reader.onload = (data) => {
+            const img = new Image();
+            img.src = data.target.result;
+            document.body.append(img);
+        };
+
+        // load the image
+        reader.readAsDataURL(file);
+    }
+    
   return (
     <div className='max-w-5xl mx-auto'>
         <h1 className='my-5 text-3xl font-bold text-center'>Event Handling in React</h1>
@@ -14,8 +31,19 @@ const EventHandling = () => {
 
         <input 
             className='w-full px-3 py-1 mt-3 border-2 border-gray-500 rounded-md'
-            onChange={ () => { console.log('Something changed'); } }
+            onChange={ ( e ) => { console.log(e.target.value); } }
         type="text" />
+
+        <input type="color" className='mt-5'
+        onChange={ ( e ) => { document.body.style.backgroundColor = e.target.value } } />
+
+        <input type="file"
+            onChange={ (e) => { console.log(e.target.files); } }        
+        />
+
+        <input type="file" 
+            onChange={ previewImage }
+        />
     </div>
   )
 }
