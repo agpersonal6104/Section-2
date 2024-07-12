847e164d1e36d51f23a8945f072329cecbd7c946
AyushG <legendsteamprivate@gmail.com>
2024-07-12 16:59:17 +0530

    React Web App Event Handing

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -7,8 +7,15 @@ const EventHandling = () => {
         <h1 className='my-5 text-3xl font-bold text-center'>Event Handling in React</h1>
         <hr />
 
-        <button className='p-3 text-white rounded-md bg-violet-400'
-        onClick={ () => { alert('Button was clicked'); } }>Click me</button>
+        <button 
+            className='p-3 mt-5 text-white rounded-md bg-violet-400'
+            onClick={ () => { alert('Button was clicked'); } }>
+        Click me</button>
+
+        <input 
+            className='w-full px-3 py-1 mt-3 border-2 border-gray-500 rounded-md'
+            onChange={ () => { console.log('Something changed'); } }
+        type="text" />
     </div>
   )
 }
