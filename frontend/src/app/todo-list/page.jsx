1a2aea28de879a9fd193454afc50b14b89fc6c31
AyushG <legendsteamprivate@gmail.com>
2024-07-14 18:06:44 +0530

    Single Page Web app

--- frontend/src/app/todo-list/page.jsx
+++ frontend/src/app/todo-list/page.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <div>
+
+        <h1 className='text-5xl font-bold text-center'>ToDo List</h1>
+
+        <div className='mt-5 border-2 rounded-md shadow'>
+
+            <div className='border-b-2 border-gray-600'>
+                
+                <input 
+                className='w-full p-3 border-2 border-blue-500 rounded' 
+                placeholder='Add a new Task'
+                type="text" />
+                
+            </div>
+            
+            <div className='p-5'></div>
+            
+        </div>
+        
+    </div>
   )
 }
 
