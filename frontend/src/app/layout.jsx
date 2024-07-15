33c537e310c422344873da79999504937dc93f41
AyushG <legendsteamprivate@gmail.com>
2024-07-15 17:33:04 +0530

    Toaster and Todop List

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Toaster position="top-right" />
         {children}
+        
       </body>
     </html>
   );
--- frontend/src/app/todo-list/page.jsx
+++ frontend/src/app/todo-list/page.jsx
@@ -1,16 +1,19 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 
-function TodoList() {
+const TodoList = () => {
+
     // const [num, setNum] = useState(10);
 
     const [taskList, setTaskList] = useState([]);
+    console.log(taskList);
+    const addTask = (e) => {
 
-    const addtask = (e) => {
         if (e.code === 'Enter') {
 
             if (!e.target.value) {
-                alert('Please enter a task');
+                alert('Enter a value to add');
                 return;
             }
 
@@ -21,63 +24,67 @@ function TodoList() {
             setTaskList([...taskList, newTask]);
 
             e.target.value = '';
+            toast.success('New Task Added Successfully');
         }
     };
 
     const deleteTask = (index) => {
         console.log(index);
+
         const temp = taskList;
         temp.splice(index, 1);
         setTaskList([...temp]);
-    };
+        toast.success('Task Deleted Successfully');
+    }
 
     const toggleComplete = (index) => {
         const temp = taskList;
         temp[index].completed = !temp[index].completed;
         setTaskList([...temp]);
-    };
+    }
 
     return (
         <div className='max-w-[80%] mx-auto'>
-
             {/* {num} */}
-            {/* <button onClick={ () => { setNum(num + 1); console.log(num); } }>add number</button> */}
-
+            {/* <button onClick={() => { setNum(num + 1); console.log(num); }}>add number</button> */}
             <h1 className='text-5xl font-bold text-center'>ToDo List</h1>
 
             <div className='mt-5 border-2 rounded-md shadow'>
-
-                <div className='border-b-2 border-gray-600'>
-
+                <div className='p-4 border-b-2 border-gray-600'>
                     <input
+                        placeholder='Add a new task'
                         className='w-full p-3 border-2 border-blue-500 rounded'
-                        placeholder='Add a new Task'
                         type="text"
-                        onKeyDown={addtask} />
-
+                        onKeyDown={addTask}
+                    />
                 </div>
 
                 <div className='p-5'>
-                    {taskList.map((task, index) => {
-                        return <div className='p-4 rounded-lg shadow-md' key={index}>
-                            <p className={ task.completed ? 'line-through' : ''} >{task.text}</p>
-                            <div className='flex justify-end gap-3 mt-3'> 
-                                <button 
-                                onClick={() => { toggleComplete(index); } } className='px-3 text-white bg-blue-500 rounded-full'>Edit</button>
-
-                                <button
-                                    onClick={() => { deleteTask(index); } }
-                                    className='px-3 text-white bg-red-500 rounded-full'
-                                >Delete</button>
+                    {
+                        taskList.map((task, index) => {
+                            return <div key={index} className='p-4 mb-5 border-2 rounded-lg shadow-md'>
+                                {task.completed ? (
+                                    <p className='px-3 text-sm text-white bg-green-500 rounded-full w-fit'>Complete</p>
+                                ) : (
+                                    <p className='px-3 text-sm text-white bg-yellow-500 rounded-full w-fit'>Pending</p>
+                                )}
+                                <p className={task.completed ? 'line-through' : ''}>{task.text}</p>
+                                <div className='flex justify-end gap-3 mt-3'>
+                                    <button
+                                        onClick={() => { toggleComplete(index) }}
+                                        className='px-3 text-white bg-blue-500 rounded-full'>Edit</button>
+                                        
+                                    <button
+                                        onClick={() => { deleteTask(index) }}
+                                        className='px-3 text-white bg-red-500 rounded-full'>Delete</button>
+                                </div>
                             </div>
-                        </div>;
-                    })}
+                        })
+                    }
                 </div>
-
             </div>
-
         </div>
-    );
+    )
 }
 
 export default TodoList;
