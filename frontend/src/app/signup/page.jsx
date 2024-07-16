90d0f84e1e87b46e38679ffd24382470407f05fe
AyushG <legendsteamprivate@gmail.com>
2024-07-16 16:50:20 +0530

    Sign Up form React

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -71,9 +71,43 @@ const SignUp = () => {
         Or
       </div>
       {/* Form */}
-      <form>
+      <form onSubmit={ signupForm.handleSubmit }>
         <div className="grid gap-y-4">
           {/* Form Group */}
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm dark:text-white"
+            >
+              Name
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="name"
+                onChange={ signupForm.handleChange }
+                value={ signupForm.values.name }
+                className="block w-full px-4 py-3 text-sm border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                required=""
+                aria-describedby="email-error"
+              />
+              <div className="absolute inset-y-0 hidden pointer-events-none end-0 pe-3">
+                <svg
+                  className="text-red-500 size-5"
+                  width={16}
+                  height={16}
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  aria-hidden="true"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                </svg>
+              </div>
+            </div>
+            <p className="hidden mt-2 text-xs text-red-600" id="email-error">
+              Please include a valid email address so we can get back to you
+            </p>
+          </div>
           <div>
             <label
               htmlFor="email"
