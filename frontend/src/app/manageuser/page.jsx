de80519f46c5ccb29de9746d9a886efa97d0ada2
AyushG <legendsteamprivate@gmail.com>
2024-08-31 20:18:15 +0530

    Manage User Table created

--- frontend/src/app/manageuser/page.jsx
+++ frontend/src/app/manageuser/page.jsx
@@ -1,24 +1,60 @@
 'use client';
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const manageUser = () => {
   
   const fetchUsersData = async () => {
+
+    const [userList, setUserList] = useState([]);
     
     const res = await axios.get('http://localhost:5000/user/getall');
     console.log(res.status);
-    console.log(res.data);
-    
+    console.table(res.data);
+    setUserList(res.data);
   }
 
   useEffect(() => {
-    
     fetchUsersData();
+  }, []);
+
+  const displayusers = () => {
+    if(userList.length === 0)
+    {
+      return <p>Loading.... Please Wait!</p>
+    }
+    else
+    {
+      return <table>
+        <thead>
+          
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>City</th>
+            </tr>
+          
+        </thead>
+        
+        <tbody>
+          {
+            userList.map((user) => {
+              return <tr key={user._id}>
+                
+                <td> {user._id} </td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.city}</td>
+                
+              </tr>
+            } )
+          }
+        </tbody>
+      </table>  
+    }
+  }
     
-  }, [])
-  
-  
   return (
     <div>
       <div className='max-w-[80%] mx-auto shadow-lg rounded-lg p-5'>
