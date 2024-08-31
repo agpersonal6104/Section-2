ce28ebc5ad2125712b2ef826f6a6d5e0291c38a6
AyushG <legendsteamprivate@gmail.com>
2024-08-31 19:26:36 +0530

    manageuser

--- frontend/src/app/manageuser/page.jsx
+++ frontend/src/app/manageuser/page.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const manageUser = () => {
   return (
-    <div>manageUser</div>
+    <div>
+      <div className='max-w-[80%] mx-auto shadow-lg rounded-lg p-5'>
+        <h1 className='mt-5 text-3xl font-bold text-center'>manageUser</h1>
+      </div>
+    </div>
   )
 }
 
