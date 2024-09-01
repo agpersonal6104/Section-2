a1cf6a708ce52e0bf4f1f8669b2378563e58b307
AyushG <legendsteamprivate@gmail.com>
2024-09-01 20:26:00 +0530

    Update Form

--- frontend/src/app/updateUser/[id]/page.jsx
+++ frontend/src/app/updateUser/[id]/page.jsx
@@ -1,16 +1,18 @@
 'use client';
 import axios from 'axios';
 import { useParams } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UpdateUser = () => {
 
     const { id } = useParams();
 
+    const [userData, setUserData] = useState(null);
+
     const getUserData = async () => {
         const res = await axios.get('http://localhost:5000/user/getbyid/' +id);
         console.log(res.data);
-        
+        setUserData(res.data);
     }
 
     useEffect(() => {
@@ -21,26 +23,43 @@ const UpdateUser = () => {
     <div>
         <div className='max-w-xl p-5 mx-auto border rounded-lg shadow'>
             <h1 className='text-2xl font-bold text-center'>Update User Details</h1>
-            
-            <form>
+
+            {
+                userData !== null ? (
+                    
+                <form>
 
                 <input
-                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' type="text"
-                id='name' placeholder='Enter your Name' />
+                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
+                type="text"
+                id='name' 
+                placeholder='Enter your Name' />
+                
                 <input
-                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' type="email"
-                id='email' placeholder='Enter your Email' />
+                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
+                type="email"
+                id='email' 
+                placeholder='Enter your Email' />
+                
                 <input
-                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' type="password"
-                id='password' placeholder='Enter your Password' />
+                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
+                type="password"
+                id='password'
+                placeholder='Enter your Password' />
+                
                 <input
-                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' type="text"
-                id='city' placeholder='Enter your City' />
+                className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
+                type="text"
+                id='city' 
+                placeholder='Enter your City' />
 
                 <button className='block p-3 mt-6 text-white bg-blue-500 rounded-lg m1-auto'>Update User</button>
-                
-                
-            </form>
+                 
+                </form>
+                    
+                ) : <p className='mt-5 text-3xl font-bold text-center text-gray-300'>Loading...</p>
+            }
+            
         </div>
     </div>
   )
