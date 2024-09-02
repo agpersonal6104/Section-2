1d898f355395c4b7fb7f7cd78b3cc8681a8d78f0
AyushG <legendsteamprivate@gmail.com>
2024-09-02 19:43:47 +0530

    Update User

--- frontend/src/app/updateUser/[id]/page.jsx
+++ frontend/src/app/updateUser/[id]/page.jsx
@@ -1,7 +1,9 @@
 'use client';
 import axios from 'axios';
+import { Formik } from 'formik';
 import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
 
 const UpdateUser = () => {
 
@@ -19,6 +21,19 @@ const UpdateUser = () => {
         getUserData();
     }, []);
 
+    const submitForm = (values) => {
+        console.log(values);
+
+        axios.put('http://localhost:5000/user/update'+id, values)
+        .then((result) => {
+            toast.success('UserUpdated Successfully!;')
+        }).catch((err) => {
+            console.log(err);
+            toast.error('Failed to update user!');
+        });
+        
+    }
+
   return (
     <div>
         <div className='max-w-xl p-5 mx-auto border rounded-lg shadow'>
@@ -26,36 +41,52 @@ const UpdateUser = () => {
 
             {
                 userData !== null ? (
-                    
-                <form>
+
+                <Formik initialValues={userData} onSubmit={submitForm}>
+                    {
+                        ( updateForm ) => { return(
+                            <form onSubmit={updateForm.handleSubmit}>
 
                 <input
                 className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
                 type="text"
-                id='name' 
+                id='name'
+                onChange={updateForm.handleChange}
+                value={updateForm.values.name}
                 placeholder='Enter your Name' />
                 
                 <input
                 className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
                 type="email"
-                id='email' 
+                id='email'
+                onChange={updateForm.handleChange}
+                value={updateForm.values.email}
                 placeholder='Enter your Email' />
                 
                 <input
                 className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
                 type="password"
                 id='password'
+                onChange={updateForm.handleChange}
+                value={updateForm.values.password}
                 placeholder='Enter your Password' />
                 
                 <input
                 className='w-full p-3 mt-5 bg-gray-100 border rounded-lg' 
                 type="text"
-                id='city' 
+                id='city'
+                onChange={updateForm.handleChange}
+                value={updateForm.values.city}
                 placeholder='Enter your City' />
 
                 <button className='block p-3 mt-6 text-white bg-blue-500 rounded-lg m1-auto'>Update User</button>
                  
                 </form>
+                        ) }
+                    }
+                </Formik>
+                    
+                
                     
                 ) : <p className='mt-5 text-3xl font-bold text-center text-gray-300'>Loading...</p>
             }
